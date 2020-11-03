a30a0875947de92f978df5a2e28ef939852255ed
Janek Janiak <janek.janiak@gmail.com>
2020-11-03 11:05:52 +0100

    Changes  InputHandler and adds it to RenderList

---
+++ src/components/InputHandler.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input  type ="number" :value="value" @input="handleInput" />
+        <!-- v-model.number="age" -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: "InputHandler",
+  data() {
+    return {
+      value: "",
+    };
+  },
+  props: {
+        itemID: Number,
+        action: Function,
+        actionName: String,
+    },
+  methods: {
+     handleInput(event) {
+      this.value = event.target.value;
+      console.log(this.value);
+      console.log(this.itemID);
+    },
+  },
+};
+</script>
