26dab031f4b6f86ae03ff9b174c8aca5df490c06
janek <janek.janiak@gmail.com>
2020-11-19 17:15:22 +0100

    in progres

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header appName="Gastro Helper"/>
-    <RecipeCostCalculator />
+    <RecipeCostCalculator/>
   </div>
 </template>
 
---
+++ src/components/IngredientLossCalc.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  div.ingredient_loss_calculator
+        
+</template>
